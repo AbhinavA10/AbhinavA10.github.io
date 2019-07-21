0fe6abff05a80ef49e93f3ff9e32419ef29aedfb
Abhi <abhiraja1012@gmail.com>
2019-07-21 01:35:19 -0400

    Use isotope filtering

--- js/AbhiScripts.js
+++ js/AbhiScripts.js
@@ -18,21 +18,32 @@ $(document).ready(function () {
     }
     //if there is a hash (modal direct link), then open it
     directLinkModal(window.location.hash);
-
-    filterSelection("all")
-
-    // Add active class to the current control button (highlight it)
-    var btnContainer = document.getElementById("btnPortfolio-container");
-    var btns = btnContainer.getElementsByClassName("btn-portfolio-filter");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function () {
-            var current = document.getElementsByClassName("btn-portfolio-filter-active");
-            current[0].className = current[0].className.replace(" btn-portfolio-filter-active", "");
-            this.className += " btn-portfolio-filter-active";
+    if (window.location.href.includes("projects.html")) {
+        // ========================================================================= //
+        //  Portfolio filtering
+        // ========================================================================= //
+        // -----  init Isotope ---- 
+        var $grid = $('.filterGrid').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
         });
+
+        // Add active class to the current control button (highlight it)
+        var btnContainer = document.getElementById("btnPortfolio-container");
+        var btns = btnContainer.getElementsByClassName("btn-portfolio-filter");
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+                //--- changing css class to update UI ---
+                var current = document.getElementsByClassName("btn-portfolio-filter-active");
+                current[0].className = current[0].className.replace(" btn-portfolio-filter-active", "");
+                this.className += " btn-portfolio-filter-active";
+                //  ----- Isotope filtering -----
+                var filterValue = $(this).attr('data-filter');
+                //console.log('filtering: ' + filterValue);
+                $grid.isotope({ filter: filterValue });
+            });
+        }
     }
-
-
 });
 
 // ========================================================================= //
@@ -41,47 +52,6 @@ $(document).ready(function () {
 function directLinkModal(hash) {
     $(hash).modal('show');
 }
-
-
-// ========================================================================= //
-//  Filtering Projects
-// ========================================================================= //
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-}
-
-// Show filtered elements
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
-        }
-    }
-}
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
-}
-
 
 // ========================================================================= //
 //  JSON for Past empolyment , from sumobot1
